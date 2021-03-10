f5d41f62a5b264889f96fdfa4f96cb304f475c11
nguyenduyhoc <duyhocnguyen97@gmail.com>
2021-03-10 18:16:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import BT3Header from './BaiTApThucHanhLayOut/BT3Header';
+import BT3Content from './BaiTApThucHanhLayOut/BT3Content';
+import BT3Footer from './BaiTApThucHanhLayOut/BT3Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <BT3Header/>
+      <BT3Content/>
+      <BT3Footer/>
     </div>
   );
 }
---
+++ src/BaiTApThucHanhLayOut/BT3ProductList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import BT3ProductItem from './BT3ProductItem'
+
+export default class BT3ProductList extends Component {
+    render() {
+        return (
+            <div>
+                <BT3ProductItem/>
+            </div>
+        )
+    }
+}
